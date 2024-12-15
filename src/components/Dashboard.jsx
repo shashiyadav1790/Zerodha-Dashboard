6f7f95ba74a37640db099ec20f67715593df6f3e
shashi yadav <shashiyadav1790@gmail.com>
2024-12-15 16:18:12 +0530

    initial commit

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,34 @@
+
+import { Route, Routes } from "react-router-dom";
+
+import Apps from "./Apps";
+import Funds from "./Fund";
+import Holdings from "./Holding";
+
+import Orders from "./Order";
+import Positions from "./Position";
+import Summary from "./Summary";
+import WatchList from "./Watchlist";
+import { GeneralContextProvider } from "./GeneralContext";
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard-container">
+      <GeneralContextProvider>
+        <WatchList />
+      </GeneralContextProvider>
+      <div className="content">
+        <Routes>
+          <Route exact path="/" element={<Summary />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/holdings" element={<Holdings />} />
+          <Route path="/positions" element={<Positions />} />
+          <Route path="/funds" element={<Funds />} />
+          <Route path="/apps" element={<Apps />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+
+
+import Dashboard from "./Dashboard";
+import TopBar from "./TopBar";
+
+const Home = () => {
+  return (
+    <>
+      <TopBar />
+      <Dashboard />
+    </>
+  );
+};
+
+export default Home;
